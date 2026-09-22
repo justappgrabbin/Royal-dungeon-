fe2631e53735c463987d648341e5e4192ccb8472
justappgrabbin <bookingalexismcmillion@gmail.com>
2026-09-21 18:02:42 -0700

    Preserve existing integrated donor: index.js

---
+++ vendor/klein-mesh-game-engine-v5.1/kmge_v5_fixed/dist/index.js
@@ -0,0 +1,57 @@
+"use strict";
+/**
+ * ============================================================
+ * KLEIN-MESH MULTI-GAME ENGINE + INGEST-GAPFILL SYSTEM
+ * Complete exports — v5 with DISEMINER eyes
+ * ============================================================
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.demo = exports.DEFAULT_CONFIG = exports.IngestGapFillPipeline = exports.MockDiffusionBackend = exports.GameNGenEngine = exports.CreativityModule = exports.HistoricalChangeModule = exports.AnalogyMysticismModule = exports.ProppLeviStraussModule = exports.AutoNovelModule = exports.AutolingModule = exports.DiseMinerModule = exports.MessySubstrate = exports.KleinEngine = exports.PixelCNNGenerator = exports.VectorQuantizer = exports.AutopoieticGenerator = exports.AtoSwitchingLayer = exports.Neocortex = exports.Hippocampus = exports.AtoEngine = exports.SensoryInputAdapter = exports.HistoricalChangeEngine = exports.AutolingGrammar = exports.DiseMinerEngine = exports.CompGramCoder = exports.KleinMeshGameEngine = exports.MeaningSynthesizer = exports.ContextEngine = exports.WitnessEngine = exports.PolarityClassifier = exports.ImpulseIngestor = exports.IngestGapFillEngine = void 0;
+// Core gap-filling engine (D1-D5)
+var core_engine_1 = require("./core-engine");
+Object.defineProperty(exports, "IngestGapFillEngine", { enumerable: true, get: function () { return core_engine_1.IngestGapFillEngine; } });
+Object.defineProperty(exports, "ImpulseIngestor", { enumerable: true, get: function () { return core_engine_1.ImpulseIngestor; } });
+Object.defineProperty(exports, "PolarityClassifier", { enumerable: true, get: function () { return core_engine_1.PolarityClassifier; } });
+Object.defineProperty(exports, "WitnessEngine", { enumerable: true, get: function () { return core_engine_1.WitnessEngine; } });
+Object.defineProperty(exports, "ContextEngine", { enumerable: true, get: function () { return core_engine_1.ContextEngine; } });
+Object.defineProperty(exports, "MeaningSynthesizer", { enumerable: true, get: function () { return core_engine_1.MeaningSynthesizer; } });
+// Klein-Mesh Multi-Game Engine (all 5 Klein papers)
+var klein_mesh_game_engine_1 = require("./klein-mesh-game-engine");
+Object.defineProperty(exports, "KleinMeshGameEngine", { enumerable: true, get: function () { return klein_mesh_game_engine_1.KleinMeshGameEngine; } });
+Object.defineProperty(exports, "CompGramCoder", { enumerable: true, get: function () { return klein_mesh_game_engine_1.CompGramCoder; } });
+Object.defineProperty(exports, "DiseMinerEngine", { enumerable: true, get: function () { return klein_mesh_game_engine_1.DiseMinerEngine; } });
+Object.defineProperty(exports, "AutolingGrammar", { enumerable: true, get: function () { return klein_mesh_game_engine_1.AutolingGrammar; } });
+Object.defineProperty(exports, "HistoricalChangeEngine", { enumerable: true, get: function () { return klein_mesh_game_engine_1.HistoricalChangeEngine; } });
+// Sensory Input Adapter (DISEMINER's eyes)
+var sensory_adapter_1 = require("./sensory-adapter");
+Object.defineProperty(exports, "SensoryInputAdapter", { enumerable: true, get: function () { return sensory_adapter_1.SensoryInputAdapter; } });
+// ATO Episodic Memory (Fayyaz et al. 2022)
+var ato_engine_1 = require("./ato-engine");
+Object.defineProperty(exports, "AtoEngine", { enumerable: true, get: function () { return ato_engine_1.AtoEngine; } });
+Object.defineProperty(exports, "Hippocampus", { enumerable: true, get: function () { return ato_engine_1.Hippocampus; } });
+Object.defineProperty(exports, "Neocortex", { enumerable: true, get: function () { return ato_engine_1.Neocortex; } });
+Object.defineProperty(exports, "AtoSwitchingLayer", { enumerable: true, get: function () { return ato_engine_1.AtoSwitchingLayer; } });
+Object.defineProperty(exports, "AutopoieticGenerator", { enumerable: true, get: function () { return ato_engine_1.AutopoieticGenerator; } });
+Object.defineProperty(exports, "VectorQuantizer", { enumerable: true, get: function () { return ato_engine_1.VectorQuantizer; } });
+Object.defineProperty(exports, "PixelCNNGenerator", { enumerable: true, get: function () { return ato_engine_1.PixelCNNGenerator; } });
+// Klein 8 tools on MESSY substrate
+var klein_full_toolkit_1 = require("./klein-full-toolkit");
+Object.defineProperty(exports, "KleinEngine", { enumerable: true, get: function () { return klein_full_toolkit_1.KleinEngine; } });
+Object.defineProperty(exports, "MessySubstrate", { enumerable: true, get: function () { return klein_full_toolkit_1.MessySubstrate; } });
+Object.defineProperty(exports, "DiseMinerModule", { enumerable: true, get: function () { return klein_full_toolkit_1.DiseMinerModule; } });
+Object.defineProperty(exports, "AutolingModule", { enumerable: true, get: function () { return klein_full_toolkit_1.AutolingModule; } });
+Object.defineProperty(exports, "AutoNovelModule", { enumerable: true, get: function () { return klein_full_toolkit_1.AutoNovelModule; } });
+Object.defineProperty(exports, "ProppLeviStraussModule", { enumerable: true, get: function () { return klein_full_toolkit_1.ProppLeviStraussModule; } });
+Object.defineProperty(exports, "AnalogyMysticismModule", { enumerable: true, get: function () { return klein_full_toolkit_1.AnalogyMysticismModule; } });
+Object.defineProperty(exports, "HistoricalChangeModule", { enumerable: true, get: function () { return klein_full_toolkit_1.HistoricalChangeModule; } });
+Object.defineProperty(exports, "CreativityModule", { enumerable: true, get: function () { return klein_full_toolkit_1.CreativityModule; } });
+// gameNgen adapter (honest mock)
+var gamengen_adapter_1 = require("./gamengen-adapter");
+Object.defineProperty(exports, "GameNGenEngine", { enumerable: true, get: function () { return gamengen_adapter_1.GameNGenEngine; } });
+Object.defineProperty(exports, "MockDiffusionBackend", { enumerable: true, get: function () { return gamengen_adapter_1.MockDiffusionBackend; } });
+// Pipeline orchestrator
+var pipeline_1 = require("./pipeline");
+Object.defineProperty(exports, "IngestGapFillPipeline", { enumerable: true, get: function () { return pipeline_1.IngestGapFillPipeline; } });
+Object.defineProperty(exports, "DEFAULT_CONFIG", { enumerable: true, get: function () { return pipeline_1.DEFAULT_CONFIG; } });
+Object.defineProperty(exports, "demo", { enumerable: true, get: function () { return pipeline_1.demo; } });
+//# sourceMappingURL=index.js.map
